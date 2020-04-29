051e2241778f212a5756d7b5f842e77b8b6c83c4
asda_house <asda@asda.ru>
2020-04-29 23:30:59 +0300

    move all to init script

--- run.sh
+++ run.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
-ring cs --instance cs_instance jdbc set-params --url jdbc:postgresql://$POSTGRES_URL?currentSchema=public
-ring cs --instance cs_instance jdbc set-params --username $POSTGRES_USER
-ring cs --instance cs_instance jdbc set-params --password $POSTGRES_PASSWORD
-ring cs --instance cs_instance jdbc-privileged set-params --url jdbc:postgresql://$POSTGRES_URL?currentSchema=public
-ring cs --instance cs_instance jdbc-privileged set-params --username $POSTGRES_USER
-ring cs --instance cs_instance jdbc-privileged set-params --password $POSTGRES_PASSWORD
-
-
-ring cs --instance cs_instance websocket set-params --hostname 0.0.0.0
-ring cs --instance cs_instance websocket set-params --port 8181
-
-ring hazelcast --instance hc_instance service start
-ring elasticsearch --instance elastic_instance service start
-ring cs --instance cs_instance service start
-
 /init.sh
 
 #======== DELETE INIT CODE ==
 sed -i "s/^\/init.sh//" /run.sh
+
+echo "start ok"
+while true; do sleep 600; done;
